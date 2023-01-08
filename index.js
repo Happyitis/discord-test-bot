22cb53a2cfd2860fb983089f1ef5c66b7437a0ca
ハッピーHappy <aayush.maharjan.499@gmail.com>
2023-01-08 13:30:45 +0545

    adding discord test bot javafiles

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+
+const prefix = '!';
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
+
+
+
+client.on("ready", ()=> {
+    console.log("i am online")
+})
+
+client.on('messageCreate', message => {
+    if (!message.content.startsWith(prefix)||message.author.bit) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    const messageArray = message.content.split(" ");
+    const argument = messageArray.slice(1);
+    const cmd = messageArray[0];
+
+if (command === 'text'){
+    message.channel.send('This is test bot')
+}
+    
+})
+
+
+
+client.login("")
